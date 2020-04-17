b7d3940f6930c30a60d75a5a77b39327c13b48d9
TheWhiteMinotaur <46830898+TheWhiteMinotaur@users.noreply.github.com>
2020-04-16 21:19:15 -0400

    Secrets

--- final-app/src/config/Fire.js
+++ final-app/src/config/Fire.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
 import 'firebase/auth';
+import apiConfig from '../apiKeys'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfO1-GFvUCptJEDq7vkXZ6ZeF5zqnGcLE",
-    authDomain: "project-d964b.firebaseapp.com",
-    databaseURL: "https://project-d964b.firebaseio.com",
+    apiKey: apiConfig.FirebaseKey,
+    authDomain: apiConfig.domain,
+    databaseURL: apiConfig.baseURL,
   };
   const fire = firebase.initializeApp(firebaseConfig);
   export default fire;
